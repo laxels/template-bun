f5d28c7d8d3dd22daa3823450aafb0c0981614a3
Axel Setyanto <laxels@gmail.com>
2026-01-24 17:06:53 -0800

    Remove zod -> convex

--- convex/examples.ts
+++ convex/examples.ts
@@ -1,35 +1,26 @@
-// See:
-// https://docs.convex.dev/functions/query-functions
-// https://stack.convex.dev/typescript-zod-function-validation#using-zod-with-convex
+// See https://docs.convex.dev/functions/query-functions
 
-import { NoOp } from "convex-helpers/server/customFunctions";
-import { zCustomMutation, zCustomQuery, zid } from "convex-helpers/server/zod4";
-import z from "zod";
-
+import { v } from "convex/values";
 import { internalMutation, mutation, query } from "./_generated/server";
 
-const zQuery = zCustomQuery(query, NoOp);
-const zMutation = zCustomMutation(mutation, NoOp);
-const zInternalMutation = zCustomMutation(internalMutation, NoOp);
-
-export const insertExample = zMutation({
+export const insertExample = mutation({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.insert("examples", { count: 0 });
   },
 });
 
-export const fetchExamples = zQuery({
+export const fetchExamples = query({
   args: {},
   handler: async (ctx) => {
     return await ctx.db.query("examples").collect();
   },
 });
 
-export const setExampleCount = zInternalMutation({
+export const setExampleCount = internalMutation({
   args: {
-    exampleId: zid("examples"),
-    count: z.number(),
+    exampleId: v.id("examples"),
+    count: v.number(),
   },
   handler: async (ctx, args) => {
     return await ctx.db.patch(args.exampleId, { count: args.count });
--- convex/schema.ts
+++ convex/schema.ts
@@ -1,16 +1,10 @@
-// See:
-// https://docs.convex.dev/database/schemas
-// https://stack.convex.dev/typescript-zod-function-validation#using-zod-with-convex
+// See https://docs.convex.dev/database/schemas
 
 import { defineSchema, defineTable } from "convex/server";
-import { zodOutputToConvex } from "convex-helpers/server/zod";
-import z from "zod";
-
-const exampleSchema = z.object({
-  count: z.number(),
-});
-export type Example = z.infer<typeof exampleSchema>;
+import { v } from "convex/values";
 
 export default defineSchema({
-  examples: defineTable(zodOutputToConvex(exampleSchema)),
+  examples: defineTable({
+    count: v.number(),
+  }),
 });
